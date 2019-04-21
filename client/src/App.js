f1a8b4f862ef669d9d3a6f01b21bd10b37e26228
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-04-20 23:36:41 -0700

    Setup react app structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+// import React, { Component } from 'react';
+
+// class Navbar extends Component{
+//   render() {
+//     return (
+
+//     )
+//   }
+// }
+
+// export default Navbar;
