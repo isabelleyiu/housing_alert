672eb69ec589d50fac5af71277e5f430f13e0ddd
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-04-20 17:49:57 -0700

    Set up passport config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@housingalert-ncmsb.mongodb.net/test?retryWrites=true`
+  mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@housingalert-ncmsb.mongodb.net/test?retryWrites=true`,
+  secretOrKey: process.env.SECRET_KEY,
 }
