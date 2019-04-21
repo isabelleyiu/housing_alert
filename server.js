79b133ad33669d7cb847e7dbefa792d090232474
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-04-20 23:09:07 -0700

    Add script for client install

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
