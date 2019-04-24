5e3ce9aa90c79f62f76a66de6ad6a9796234d5a7
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-04-23 20:36:36 -0700

    Implement function to text all users

--- server/controllers/twilio-controller.js
+++ server/controllers/twilio-controller.js
@@ -1,13 +1,33 @@
 require('dotenv').config();
+const db = require("../models");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);;
+const client = require('twilio')(accountSid, authToken);
+const authy = require('authy')(process.env.AUTHY_API_KEY);
 
-client.lookups.phoneNumbers('+15107314004').get((error, number) => {
-  console.log(number);
-  console.log(number.national_format);
-  console.log(number.country_code);
 
-  // This will sometimes be null
-  console.log(number.caller_name);
-});
+const textAllUserPhoneNumber = () => {
+  db.User.findAll()
+  .then(users => {
+    users.forEach(user => {
+      const message = client.messages.create({
+        body: 'Thank you for signing up for Housing Alert!',
+        from: '+14152002988',
+        to: `+1${user.phone}`
+      })
+      .then(message =>  console.log(message.status))
+      .done();
+    })
+    .catch(err => console.log(err))
+  });
+}
+
+// works here but invalid API key in user-controller
+// authy
+//     .phones()
+//     .verification_start('5107314004', '1', 'sms', function(err, resp) {
+//       if (err) {
+//       console.log(err);
+//       }
+//       console.log(resp);
+//     });
