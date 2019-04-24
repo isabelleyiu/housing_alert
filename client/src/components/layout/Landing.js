d170fbcba395c94cf709e7edff6386956cfbd574
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-04-23 20:38:03 -0700

    Add input field for user to verify sms code

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,17 +5,19 @@ class Landing extends Component{
     super(props);
     this.state = {
       phone: '',
-      message: ''
+      message: '',
+      verificationCode: '',
     }
   }
   handleChange = e => {
     this.setState({
-      phone: e.target.value
+      [e.target.name]: e.target.value
     });
   }
-  handleSubmit = e => {
+  registerPhone = e => {
     e.preventDefault();
     this.savePhoneNumber(this.state.phone);
+    this.sendVerification(this.state.phone)
   }
   savePhoneNumber = (phone) => {
     fetch('api/user', {
@@ -31,17 +33,63 @@ class Landing extends Component{
     })
     .catch(err => console.log(err))
   }
+  sendVerification = (phone) => {
+    fetch('api/user/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({phone: phone})
+    })
+    .then(res => res.json())
+    .then(newNumber => {
+      this.setState({ message: newNumber.message })
+    })
+    .catch(err => console.log(err))
+  }
+  verifyPhone = verificationCode => {
+    // call controller.verfy
+    fetch('api/user/verify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        phone: this.state.phone,
+        verificationCode: verificationCode,
+      })
+    })
+    .then(res => res.json())
+    .then(verification => {
+      this.setState({ message: verification.message })
+    })
+    .catch(err => console.log(err))
+  }
   render() {
     return (
       <div className="landing">
         <h1>Housing Alert</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" 
+
+        <form onSubmit={this.registerPhone}>
+          <input 
+          type="text" 
+          name="phone"
           placeholder="Enter Your Phone Number" 
           onChange={this.handleChange}></input>
           <button type="submit">Submit</button>
         </form>
+
         {this.state.message? <p>{this.state.message}</p> : null}
+
+        <form onSubmit={this.verifyPhone}>
+          <input 
+          type="text" 
+          name="verificationCode"
+          placeholder="Enter Your Verification Code" 
+          onChange={this.handleChange}></input>
+          <button type="submit">Submit</button>
+        </form>
+
       </div>
     )
   }
