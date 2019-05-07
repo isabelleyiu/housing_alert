000bc0e9efd2c1dae1a49cb6cf838f4414ad9972
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-05-07 11:43:15 -0700

    Link housing card apply button to DAHLIApage

--- client/src/components/HousingCard.js
+++ client/src/components/HousingCard.js
@@ -3,64 +3,70 @@ import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 import moment from 'moment';
 import PropTypes from "prop-types";
 
-class HousingCard extends Component{
+class HousingCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    
+
     };
   }
-  
-  render() {
-    const { Building_Name, Building_Street_Address, Building_Zip_Code, imageURL, Tenure, unitSummaries, Application_Due_Date } = this.props.housing;
 
-    const { unitType, minMonthlyRent, maxMonthlyRent, minPercentIncome, maxPercentIncome, minPriceWithParking, maxPriceWithParking, minPriceWithoutParking,maxPriceWithoutParking } = unitSummaries.general[0];
+  render() {
+    const { Building_Name, Building_Street_Address, Building_Zip_Code, imageURL, Tenure, unitSummaries, Application_Due_Date, listingID } = this.props.housing;
+
+    const { unitType, minMonthlyRent, maxMonthlyRent, minPercentIncome, maxPercentIncome, minPriceWithParking, maxPriceWithParking, minPriceWithoutParking, maxPriceWithoutParking } = unitSummaries.general[0];
     let rent = null;
     let price = null;
 
-    if(Tenure === "Re-rental") {
-      if(minMonthlyRent === null && maxMonthlyRent === null){
+    if (Tenure === "Re-rental") {
+      if (minMonthlyRent === null && maxMonthlyRent === null) {
         rent = `${minPercentIncome}% of Income`;
-      } else if(minPercentIncome !== maxPercentIncome) {
+      } else if (minPercentIncome !== maxPercentIncome) {
         rent = `${minPercentIncome}% - ${maxPercentIncome}% of Income`;
-      } else if(minMonthlyRent !== maxMonthlyRent) {
+      } else if (minMonthlyRent !== maxMonthlyRent) {
         rent = `$${minMonthlyRent} - $${maxMonthlyRent}`
       } else {
         rent = `$${minMonthlyRent}`;
+      }
     }
-  }
-    
-    if(Tenure === "Resale") {
-      if(minPriceWithParking !== maxPriceWithParking) {
+
+    if (Tenure === "Resale") {
+      if (minPriceWithParking !== maxPriceWithParking) {
         price = `$${minPriceWithParking} - $${maxPriceWithParking}`;
-      } else if(minPriceWithParking) {
+      } else if (minPriceWithParking) {
         price = `$${minPriceWithParking}`;
-      } else if(minPriceWithoutParking !== maxPriceWithoutParking){
+      } else if (minPriceWithoutParking !== maxPriceWithoutParking) {
         price = `$${minPriceWithoutParking} - $${maxPriceWithoutParking}`;
       } else {
         price = `$${minPriceWithoutParking}`;
       }
     }
-    
+
     return (
       <div>
-        <Card style={{ width: "24rem", marginBottom: "100px"}}>
-          <Card.Img variant="top" src={ imageURL } style={{ height: "300px" }} />
-          <Card.Body style={{ height: "600px"}}>
-            <Card.Title>{ Building_Name }</Card.Title>
+        <Card style={{ width: "24rem", marginBottom: "100px" }}>
+          <Card.Img variant="top" src={imageURL} style={{ height: "300px" }} />
+          <Card.Body style={{ height: "600px" }}>
+            <Card.Title>{Building_Name}</Card.Title>
             <Card.Text>
             </Card.Text>
             <ListGroup className="list-group-flush">
-            <ListGroupItem>Address: { Building_Street_Address }, San Francisco, CA { Building_Zip_Code }</ListGroupItem>
-            <ListGroupItem>Tenure: { Tenure }</ListGroupItem>
-            <ListGroupItem>Unit Type: { unitType }</ListGroupItem>
-            <ListGroupItem>{rent? 'Monthly Rent': 'Price'}: {rent? rent: price}</ListGroupItem>
-            <ListGroupItem>Application Due: { moment(Application_Due_Date).format('MMMM Do YYYY, h:mm a') }</ListGroupItem>
+              <ListGroupItem>Address: {Building_Street_Address}, San Francisco, CA {Building_Zip_Code}</ListGroupItem>
+              <ListGroupItem>Tenure: {Tenure}</ListGroupItem>
+              <ListGroupItem>Unit Type: {unitType}</ListGroupItem>
+              <ListGroupItem>{rent ? 'Monthly Rent' : 'Price'}: {rent ? rent : price}</ListGroupItem>
+              <ListGroupItem>Application Due: {moment(Application_Due_Date).format('MMMM Do YYYY, h:mm a')}</ListGroupItem>
             </ListGroup>
-            <Button variant="success">Apply</Button>
+            <Button variant="success">
+              <a
+                target='_blank'
+                href={`https://housing.sfgov.org/listings/${listingID}`}
+                style={{ color: "inherit", textDecoration: "none" }}
+              >Apply</a>
+            </Button>
           </Card.Body>
         </Card>
-      </div>
+      </div >
     )
   }
 }
