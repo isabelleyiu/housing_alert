8bdfc675bb77a03077291516df660af46d767fc8
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-05-13 17:23:08 -0700

    Add Enzyme test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdaptor from 'enzyme-adapter-react-16';
+
 import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdaptor() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+m;
