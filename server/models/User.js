53d1cd129c21bbc2df3526f6345de198996b4ad5
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-06-14 15:09:59 -0700

    Fix bug with age setter

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const bcrypt = require('bcryptjs');
-const moment = require('moment')
+const moment = require('moment');
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -15,9 +15,9 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       isUnique: true,
       validate: {
-        isPhoneNumber(value) { 
+        isPhoneNumber(value) {
           const allTenNum = /^\d{10}$/.test(value);
-          if(!allTenNum) {
+          if (!allTenNum) {
             throw new Error('Phone number has to be 10 digits');
           }
         }
@@ -31,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isAlpha:  {
+        isAlpha: {
           args: true,
           msg: 'Please only enter letters for First Name'
         }
@@ -41,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isAlpha:  {
+        isAlpha: {
           args: true,
           msg: 'Please only enter letters for Last Name'
         }
@@ -50,18 +50,14 @@ module.exports = (sequelize, DataTypes) => {
     DOB: {
       type: DataTypes.DATEONLY,
       allowNull: false,
-      set: function calculateAge(birthday) {
-        this.setDataValue('DOB', birthday); 
-        this.setDataValue('age', moment().diff(moment(birthday, 'YYYYMMDD'), 'years'));
-       },
       validate: {
-        isDate:  {
+        isDate: {
           args: true,
           msg: 'Please enter date in YYYY-MM-DD format'
         }
       }
     },
-    age : {
+    age: {
       type: DataTypes.INTEGER
     },
     householdSize: {
@@ -98,20 +94,24 @@ module.exports = (sequelize, DataTypes) => {
     }
   });
 
-  User.associate = (models) => {
+  User.associate = models => {
     User.hasOne(models.Phone, {
       sourceKey: 'uuid',
       foreignKey: 'userUUID',
       onDelete: 'cascade'
-    })
-  }
+    });
+  };
 
   // hash password before inserting into db
   User.beforeCreate((user, options, done) => {
-    const hashedPassword = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10))
-    user.password = hashedPassword
-  })
+    const hashedPassword = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10)
+    );
+    user.password = hashedPassword;
+
+    user.age = moment().diff(moment(user.DOB, 'YYYYMMDD'), 'years');
+  });
 
   return User;
-}
-
+};
